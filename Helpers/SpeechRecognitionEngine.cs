3dddfbb85172e85977eb0a38623e3c993b95fb47
mani1911 <mail2aj19@gmail.com>
2025-06-28 22:38:18 +0530

    feat: added speech recognition and control

--- Helpers/SpeechRecognitionEngine.cs
+++ Helpers/SpeechRecognitionEngine.cs
@@ -1,36 +1,82 @@
-﻿using System.Speech.Recognition;
-namespace SpeechRecognitionApp
+﻿using Microsoft.CognitiveServices.Speech;
+
+
+namespace Helpers
 {
     class SpeechRecognition
     {
-        static bool shouldExit = false;
-        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        private string _apikey;
+        private string _region;
+        private SpeechRecognizer _recognizer;
+
+        public SpeechRecognition(string apikey, string region)
         {
-            Console.WriteLine("Recognized text: " + e.Result.Text);
+            _apikey = apikey;
+            _region = region;
+            _recognizer = new SpeechRecognizer(SpeechConfig.FromSubscription(apikey, region));  
         }
-        public static void Run()
+
+        async public Task StartSpeechRecognition()
         {
 
-            foreach (var device in SpeechRecognitionEngine.InstalledRecognizers())
-                Console.WriteLine("Installed recognizer: " + device.Culture);
+            await _recognizer.StartContinuousRecognitionAsync();
 
-            SpeechRecognitionEngine recognizer =
-              new SpeechRecognitionEngine(
-                new System.Globalization.CultureInfo("en-US"));
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
 
-            recognizer.LoadGrammar(new DictationGrammar()); // Open-ended speech
-
-            recognizer.SetInputToDefaultAudioDevice(); // Use system mic
-
-            recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
-
-            recognizer.RecognizeAsync(RecognizeMode.Multiple);
-
-            // Keep app alive while recognizing
-            while (!shouldExit)
-            {
-                Console.ReadLine();
-            }
+            await _recognizer.StopContinuousRecognitionAsync();
         }
+
+        public SpeechRecognizer GetSpeechRecognizer()
+        {
+            return _recognizer;
+        }
+
+        //public async Task Run()
+        //{
+        //    var config = SpeechConfig.FromSubscription(_apikey, _region);
+
+        //    using var recognizer = new SpeechRecognizer(config);
+
+        //    recognizer.Recognizing += (s, e) =>
+        //    {
+        //        Console.WriteLine($"Recognizing: {e.Result.Text}");
+        //    };
+
+        //    recognizer.Recognized += (s, e) =>
+        //    {
+        //        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+        //        {
+        //            Console.WriteLine($"Recognized: {e.Result.Text}");
+        //        }
+        //        else if (e.Result.Reason == ResultReason.NoMatch)
+        //        {
+        //            Console.WriteLine("No speech could be recognized.");
+        //        }
+        //    };
+
+        //    recognizer.Canceled += (s, e) =>
+        //    {
+        //        Console.WriteLine($"Canceled: Reason={e.Reason}");
+        //        if (e.Reason == CancellationReason.Error)
+        //        {
+        //            Console.WriteLine($"Error details: {e.ErrorDetails}");
+        //        }
+        //    };
+
+        //    recognizer.SessionStopped += (s, e) =>
+        //    {
+        //        Console.WriteLine("Session stopped.");
+        //    };
+
+        //    Console.WriteLine("Speak into your microphone.");
+        //    await recognizer.StartContinuousRecognitionAsync();
+
+        //    Console.WriteLine("Press any key to stop...");
+        //    Console.ReadKey();
+
+        //    await recognizer.StopContinuousRecognitionAsync();
+        //}
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,20 +1,25 @@
-﻿using Microsoft.SemanticKernel;
+﻿using DotNetEnv;
+using Helpers;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using WinAI.Plugins;
-using DotNetEnv;
 public class Program
 {
     public static async Task Main()
     {
-        //SpeechRecognitionApp.SpeechRecognition.Run();
-        //return;
+
 
         // get env path
         var basePath = AppContext.BaseDirectory;
         var envPath = Path.Combine(basePath, "..", "..", "..", ".env");
-
         Env.Load(envPath);
+
+        var speechRecognition = new SpeechRecognition(Environment.GetEnvironmentVariable("SPEECH_API_KEY"), Environment.GetEnvironmentVariable("SPEECH_REGION"));
+
+        // get the services
+        var recognizer = speechRecognition.GetSpeechRecognizer();
         var builder = Kernel.CreateBuilder();
 
         builder.AddAzureOpenAIChatCompletion(
@@ -32,54 +37,68 @@ public class Program
         var chatService = kernel.GetRequiredService<IChatCompletionService>();
         ChatHistory chatMessages = new ChatHistory();
 
-
-        //var spot = new SpotifyPlugin();
-        //Console.WriteLine(spot.OpenSpotify());
-
-        //Thread.Sleep(2000);
-
-        //Console.WriteLine(spot.PlayMusic());
-
-        //Thread.Sleep(2000);
-
-        //Console.WriteLine(spot.PlayNextMusic());
-
-        //Thread.Sleep(2000);
-
-        //Console.WriteLine(spot.PlayPreviousMusic());
-
-        //Thread.Sleep(2000);
-
-        //Console.WriteLine(spot.CloseSpotify());
-
-        //Thread.Sleep(2000);
-
-        //Console.WriteLine(spot.CloseSpotify());
-
-        while (true)
+        recognizer.Recognized += async (s, e) =>
         {
-            Console.Write("User:> ");
-            var input = Console.ReadLine();
-
-            chatMessages.AddUserMessage(input);
-
-            var completion = chatService.GetStreamingChatMessageContentsAsync(
-                chatHistory: chatMessages,
-                executionSettings: new OpenAIPromptExecutionSettings
-                {
-                    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
-                },
-                kernel: kernel);
-
-            string fullMessage = "";
-            await foreach (var message in completion)
+            if (e.Result.Reason == ResultReason.RecognizedSpeech)
             {
-                Console.Write(message.Content);
-                fullMessage += message.Content;
-            }
+                //Console.WriteLine($"Recognized: {e.Result.Text}");
 
-            chatMessages.AddAssistantMessage(fullMessage);
-            Console.WriteLine();
-        }
+                var input = e.Result.Text.Trim();
+                chatMessages.AddUserMessage(input);
+
+                var completion = chatService.GetStreamingChatMessageContentsAsync(
+                    chatHistory: chatMessages,
+                    executionSettings: new OpenAIPromptExecutionSettings
+                    {
+                        ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+                    },
+                    kernel: kernel);
+
+                string fullMessage = "";
+                await foreach (var message in completion)
+                {
+                    Console.Write(message.Content);
+                    fullMessage += message.Content;
+                }
+
+                chatMessages.AddAssistantMessage(fullMessage);
+                Console.WriteLine();
+            }
+            else if (e.Result.Reason == ResultReason.NoMatch)
+            {
+                Console.WriteLine("No speech could be recognized.");
+            }
+        };
+
+        await speechRecognition.StartSpeechRecognition();
+
+
+        return;
+
+        //while (true)
+        //{
+        //    Console.Write("User:> ");
+        //    var input = Console.ReadLine();
+
+        //    chatMessages.AddUserMessage(input);
+
+        //    var completion = chatService.GetStreamingChatMessageContentsAsync(
+        //        chatHistory: chatMessages,
+        //        executionSettings: new OpenAIPromptExecutionSettings
+        //        {
+        //            ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+        //        },
+        //        kernel: kernel);
+
+        //    string fullMessage = "";
+        //    await foreach (var message in completion)
+        //    {
+        //        Console.Write(message.Content);
+        //        fullMessage += message.Content;
+        //    }
+
+        //    chatMessages.AddAssistantMessage(fullMessage);
+        //    Console.WriteLine();
+        //}
     }
 }
